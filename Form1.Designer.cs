bce3da9a4a3b092a295645cf307bac032b139e1c
Kylie Banman <339780520@GoTVDSB.ca>
2025-04-01 14:16:25 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Forms_assignment_1
+{
+    partial class FormGoodBad
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FormGoodBad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "FormGoodBad";
+            this.Text = "Assignment one - good/bad";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
